1a5c4ea7f722fc7547253b62d54f93bcebb1cc3c
Austin Lipe <alipe1998@gmail.com>
2024-08-11 15:10:49 -0500

    update `evaluate_clsuters.py`

--- src/evaluate_clusters.py
+++ src/evaluate_clusters.py
@@ -31,32 +31,26 @@ def read_csv_from_s3(s3_url: str) -> pd.DataFrame:
     Returns:
     - pandas DataFrame object
     '''
-    # Parse the S3 URL
+    # parse the S3 URL
     s3_components = s3_url.replace("s3://", "").split("/")
     bucket_name = s3_components[0]
     prefix = "/".join(s3_components[1:])
-
-    # Initialize S3 client using credentials from config.py
+    # call s3 object
     s3 = boto3.client(
         's3',
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
-
-    # List objects within the specified S3 bucket and prefix
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
 
-    # Combine all CSV files into a single DataFrame
+    # concat csv files
     all_dataframes = []
-
     for obj in response.get('Contents', []):
         if obj['Key'].endswith('.csv'):
             csv_obj = s3.get_object(Bucket=bucket_name, Key=obj['Key'])
             csv_content = csv_obj['Body'].read().decode('utf-8')
             df = pd.read_csv(StringIO(csv_content))
             all_dataframes.append(df)
-
-    # Concatenate all DataFrames
     combined_df = pd.concat(all_dataframes, ignore_index=True)
 
     return combined_df
@@ -73,31 +67,24 @@ def plot_highest_fatality(crash_data_df: pd.DataFrame, rank: int, distance: int,
     - distance (int): Select clusters that are located within n mile(s) of the specified cluster centroid
     - save_plot (str): If a path is specified then the plot is saved in the specified directory.
     '''
-    # Sort clusters by fatality_rate in descending order
+    # sort clusters by fatality_rate in descending order
     sorted_df = crash_data_df.sort_values(by='fatality_rate', ascending=False).reset_index(drop=True)
-
-    # Get the nth ranked cluster
-    nth_cluster = sorted_df.iloc[rank - 1]
+    nth_cluster = sorted_df.iloc[rank - 1] # retrieve nth ranked cluster
     centroid_lat = nth_cluster['centroid_latitude']
     centroid_lon = nth_cluster['centroid_longitude']
     print(f"Fatality Rate: {round(nth_cluster['fatality_rate'], 2)}")
-
-    # Calculate distance from nth cluster to all clusters
+    # get distance from nth cluster
     crash_data_df['distance'] = ((crash_data_df['centroid_latitude'] - centroid_lat)**2 +
                                  (crash_data_df['centroid_longitude'] - centroid_lon)**2).pow(0.5) * 69.0  # 69 miles per degree
 
-    # Select clusters that are located within n mile(s) of the specified cluster centroid
     nearby_clusters = crash_data_df[crash_data_df['distance'] <= distance]
-
-    # Normalize fatality rates to a scale from 0 (min) to 1 (max) for color mapping
+    # get range of fatality rates and normalize
     min_fatality = nearby_clusters['fatality_rate'].min()
     max_fatality = nearby_clusters['fatality_rate'].max()
     nearby_clusters['color_value'] = (nearby_clusters['fatality_rate'] - min_fatality) / (max_fatality - min_fatality)
 
-    # Create a colormap
+    # create plot using folium
     colormap = mcolors.LinearSegmentedColormap.from_list("green_red", ["green", "red"])
-
-    # Plot using Folium
     folium_map = Map(location=[centroid_lat, centroid_lon], zoom_start=14)
 
     for _, row in nearby_clusters.iterrows():
@@ -112,8 +99,7 @@ def plot_highest_fatality(crash_data_df: pd.DataFrame, rank: int, distance: int,
     
     if save_path:
         file_path = Path(save_path)
-        folium_map.save(file_path / f"high_fatality_clusters_{rank}.html")  # Save the map as an HTML file
-    # Display the map
+        folium_map.save(file_path / f"high_fatality_clusters_{rank}.html")  # save the map as an HTML file
     display(folium_map)
 
     return folium_map
